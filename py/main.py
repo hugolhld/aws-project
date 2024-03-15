f1d980b1ed439847ed1452ea310dd658b9d6ba94
Zoltrak <a_lanyan@hetic.eu>
2024-03-15 09:42:22 +0100

    feat(main.py):creation of spark server and initialization of cloudwatch

--- py/main.py
+++ py/main.py
@@ -1,17 +1,39 @@
 from pyspark import SparkContext, SparkConf
 from fastapi import FastAPI
+import boto3
 
 app = FastAPI()
 
+# Initialisez le client CloudWatch
+cloudwatch = boto3.client('cloudwatch')
+
 @app.get("/")
 def main():
-
+    # Configuration de Spark
     conf = SparkConf().setAppName("HelloWorld")
+    conf.set("spark.eventLog.enabled", "true")
+    conf.set("spark.eventLog.dir", "./monitoring")
+
+    # Initialisation du contexte Spark
     sc = SparkContext(conf=conf)
 
+    # Logique de votre application Spark
     rdd = sc.parallelize(["Hello, World!"])    
     print(rdd.collect())
 
+    # Envoi d'une métrique à CloudWatch
+    cloudwatch.put_metric_data(
+        Namespace='VotreNamespace',
+        MetricData=[
+            {
+                'MetricName': 'NombreDeTravauxSpark',
+                'Value': 1,
+                'Unit': 'Count'
+            }
+        ]
+    )
+
+    # Arrêt du contexte Spark
     sc.stop()
 
 @app.get("/hello")
